6ff3730f27dff0727cf8d41e8c5890fd72317e45
Tomasheva Christina <kunavichkm@gmail.com>
2022-03-21 12:31:14 +0300

    feat:add new functions

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 "use strict";
-var size = 8;
-var pictureWidth = 54;
-var moves = [
-  [0,0],
-  [0,1],
-  [0,2],
-];
-var board = [];
+let size = 8;
+let pictureWidth = 54;
+let moves = [];
+let board = [];
 
-for (var i = 0; i < size; i++) {
+for (let i = 0; i < size; i++) {
   board[i] = [];
-  for (var j = 0; j < size; j++) {
+  for (let j = 0; j < size; j++) {
     board[i][j] = 0;
   }
 }
 
 function buildBoard(size) {
-  var table = document.getElementById("CHESSTABLE");
+  let table = document.getElementById("CHESSTABLE");
   table.style.position = "relative";
-  for (var i = 0; i < size; i++) {
-    var tr = document.createElement("tr");
-    for (var j = 0; j < size; j++) {
-      var td = document.createElement("td");
+  for (let i = 0; i < size; i++) {
+    let tr = document.createElement("tr");
+    for (let j = 0; j < size; j++) {
+      let td = document.createElement("td");
       tr.appendChild(td);
     }
     table.appendChild(tr);
@@ -30,20 +26,111 @@ function buildBoard(size) {
 
 buildBoard(size);
 
-/*function writeDownSolutions() {
-  var arr = [];
-  for (var i = 0; i < size; i++) {
-    for (var j = 0; j < size; j++) {
-      if (board[i][j] == -1) {
-        arr.push([i, j]);
-      }
-    }
+chooseStartPosition();
+
+function randomDiap(n,m) {
+  return Math.floor(Math.random()*(m-n+1))+n;
+}
+function chooseStartPosition () {
+  let startPosition = randomDiap(1,4);
+  switch (startPosition) {
+    case 1: 
+      moves.push([0,0]);
+      break;
+
+    case 2: 
+      moves.push([0,7]);
+      break; 
+
+    case 3: 
+      moves.push([7,0]);
+      break; 
+
+    case 4: 
+      moves.push([7,7]);
+      break;          
   }
-  solutions.push(arr);
 }
 
-function findSolution(i) {
-  for (var j = 0; j < size; j++) {
+let i = moves[0][0];
+let j = moves[0][1];
+
+let possibleMoves = [
+  [i-1,j+2],
+  [i+1,j+2],
+  [i+2,j+1],
+  [i+2,j-1],
+  [i+1,j-2],
+  [i-1,j-2],
+  [i-2,j-1],
+  [i-2,j+1],
+];
+
+ function findPossibleMoves(v,i,a) {
+   return (v[0]>=0&&v[0]<size&&v[1]>=0&&v[1]<size&&board[v[0]][v[1]]!=1);
+ }
+
+ function countNextMoves(array) {
+    return array.length
+ }
+
+ let nextMoves;
+ let nextCoords;
+
+
+ function tryMove() {
+  board[i][j] = 1;
+  nextMoves = possibleMoves.filter(findPossibleMoves);
+  let numberOfNextMoves = [];
+  let nextVars;
+  function whatToDoNext() {
+    for (let i=0;i<nextMoves.length;i++) {
+      let nextX = nextMoves[i][1];
+      let nextY = nextMoves[i][0];
+      nextVars = [
+        [nextY-1,nextX+2],
+        [nextY+1,nextX+2],
+        [nextY+2,nextX+1],
+        [nextY+2,nextX-1],
+        [nextY+1,nextX-2],
+        [nextY-1,nextX-2],
+        [nextY-2,nextX-1],
+        [nextY-2,nextX+1],
+      ];
+      let nextNextMoves = nextVars.filter(findPossibleMoves);
+      console.log(nextNextMoves);
+      numberOfNextMoves.push(nextNextMoves.length);
+    }
+    return numberOfNextMoves;
+  }
+  numberOfNextMoves = whatToDoNext();
+  console.log(numberOfNextMoves);
+
+  let length =  numberOfNextMoves.length;
+  let min = numberOfNextMoves[0];
+  let index = 0;
+  while(length--) {
+    if(numberOfNextMoves[length] < min) {
+      min = numberOfNextMoves[length];
+      index = length;
+    }
+  }
+  console.log(numberOfNextMoves[index]);
+
+  //board[nextVars[index][0]][nextVars[index][1]] = 1;
+}
+
+tryMove();
+console.log(board);
+//console.log(board);
+//console.log(nextMoves);
+
+//console.log(moves);
+//console.log(possibleMoves);
+//console.log(nextMoves);
+
+/*function findSolution(i) {
+  for (let j = 0; j < size; j++) {
     if (board[i][j] == 0) {
       tryQueen(i, j);
 
@@ -57,12 +144,12 @@ function findSolution(i) {
     }
   }
 }
-findSolution(0);
+findSolution(0);*/
 
-function tryQueen(i, j) {
-  var x = i - j;
-  var y = i + j;
-  for (var k = 0; k < size; k++) {
+/*function tryQueen(i, j) {
+  let x = i - j;
+  let y = i + j;
+  for (let k = 0; k < size; k++) {
     board[i][k] += 1;
     board[k][j] += 1;
     if (k + x >= 0 && k + x < size) {
@@ -75,10 +162,10 @@ function tryQueen(i, j) {
   board[i][j] = -1;
 }
 
-function removeQueen(i, j) {
-  var x = i - j;
-  var y = i + j;
-  for (var k = 0; k < size; k++) {
+/*function removeQueen(i, j) {
+  let x = i - j;
+  let y = i + j;
+  for (let k = 0; k < size; k++) {
     board[i][k] -= 1;
     board[k][j] -= 1;
     if (k + x >= 0 && k + x < size) {
@@ -89,90 +176,60 @@ function removeQueen(i, j) {
     }
   }
   board[i][j] = 0;
-}
-
-function showSolutions(e) {
-  e = e || window.event;
-  e.preventDefault();
-  var span1 = document.getElementById("span1");
-  var div = document.getElementById("div");
-  div.style.display = "block";
-  var string = "";
-  for (var i = 0; i < solutions.length; i++) {
-    var str = "";
-    for (var j = 0; j < size; j++) {
-      str += `(${solutions[i][j]})`;
-    }
-    string += `<br/> ${i + 1}) ${str}`;
-  }
-  span1.textContent = `Количество найденных вариантов:\n ${solutions.length}`;
-  div.innerHTML = string;
 }*/
 let table = document.getElementById("CHESSTABLE");
+let knight = {
+  size: 54,
+  position: "absolute",
+  src: "knight.png",
+  id: "knight",
+  posX: moves[0][1],
+  posY: moves[0][0],
+  isCreated: false,
+  create: function () {
+    if (!this.isCreated) {
+      let img = document.createElement("img");
+      img.setAttribute("src", this.src);
+      img.setAttribute("id", this.id);
+      img.style.position = this.position;
+      table.appendChild(img);
+    }
+    this.isCreated = true;
+  },
+  set: function () {
+    let img = document.getElementById("knight");
+    img.style.left = this.size * this.posX + "px";
+    img.style.top = this.size * this.posY + "px";
+  },
+};
+knight.create();
+knight.set();
+let timer = 0;
+let k = 0;
 function showMoves(e) {
   e = e || window.event;
   e.preventDefault();
-  createKnight();
-  setKnight();
-  /*for (let k = 0; k < size; k++) {
-    for (let l = 0; l < size; l++) {
-      var td = table.rows[k].cells[l];
-      td.style.backgroundColor = "";
-    }*/
-  }
   
-  
-  function createKnight() {
-    let img = document.createElement("img");
-    img.setAttribute("src", "knight.png");
-    img.setAttribute("id", "knight");
-    img.style.position = "absolute";
-    table.appendChild(img);
-  }
 
-  function setKnight() {
-    let img = document.getElementById("knight");
-    img.style.left = pictureWidth * moves[0][1] + "px";
-    img.style.top = pictureWidth * moves[0][0] + "px";
-  }
-
-   let knight = {
-     size:54,
-     position:absolute,
-     posX:moves[1],
-     posY:moves[0],
+    if (timer) {
+      clearInterval(timer);
+      timer = 0;
+    }
+    timer = setInterval(move, 500); 
     
+    function move() {
+      if (k==moves.length-1) {
+        clearInterval(timer);
+        timer = 0;
+      };
+      knight.posX = moves[k][1];
+      knight.posY = moves[k][0];
+      console.log(knight.posY, knight.posX);
+      table.rows[knight.posY].cells[knight.posX].textContent = `${k + 1}`;
+      knight.set();
+      k++;
+    } 
+}
 
-   } 
-/* function showThreatPlaces(e) {
-  e = e || window.event;
-  e.preventDefault();
-  var self = this;
-  var j = self.offsetLeft / pictureWidth;
-  var i = self.offsetTop / pictureWidth;
-  var x = i - j;
-  var y = i + j;
-  var table = document.getElementById("CHESSTABLE");
-  for (var k = 0; k < size; k++) {
-    for (var l = 0; l < size; l++) {
-      var td = table.rows[k].cells[l];
-      td.style.backgroundColor = "";
-    }
-  }
-  for (var k = 0; k < size; k++) {
-    var tdh = table.rows[i].cells[k];
-    var tdv = table.rows[k].cells[j];
-    tdh.style.backgroundColor = "#A9CCEE";
-    tdv.style.backgroundColor = "#A9CCEE";
-    if (k + x >= 0 && k + x < size) {
-      var diag1 = table.rows[k + x].cells[k];
-    }
-    if (y - k >= 0 && y - k < size) {
-      var diag2 = table.rows[y - k].cells[k];
-    }
-    if (diag1) diag1.style.backgroundColor = "#A9CCEE";
-    if (diag2) diag2.style.backgroundColor = "#A9CCEE";
-  }
 
-}*/
 
